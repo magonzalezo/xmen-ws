c4076cac01e61dd480ffe534fb2a8f24b40fcd95
magonzalezo <miguel.gonzalez.ordonez@gmail.com>
2021-05-31 18:21:02 -0500

    Change db from H2 to MySql and add google cloud config

--- src/main/java/com/magonzalezo/xmen/entities/StatEntity.java
+++ src/main/java/com/magonzalezo/xmen/entities/StatEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="stats")
+@Table(name="stats", schema="mutantdatabase")
 public class StatEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
